19dda7e617e522312fa4cefc8cb37d8fd992410d
kbaooo <kbao122003@gmail.com>
2025-07-10 17:10:21 +0700

    fix/deploy bug: type

--- src/app/(main)/lien-he/page.tsx
+++ src/app/(main)/lien-he/page.tsx
@@ -33,7 +33,7 @@ export default function ContactPage() {
             </div>
             <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-700">Tin nhắn của bạn</label>
-                <textarea className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" rows="4" placeholder="Nhập tin nhắn của bạn" required></textarea>
+                <textarea className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" rows={4} placeholder="Nhập tin nhắn của bạn" required></textarea>
             </div>
             <button type="submit" className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
                 Gửi tin nhắn
